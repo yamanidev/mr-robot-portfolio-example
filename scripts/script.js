e9837ca63650f69464e8e5dc014b3dd524c9c535
yamanidev <elamine.yamani@gmail.com>
2021-08-25 15:19:58 +0100

    Added jQuery smooth scrolling to the page

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+$(".top-nav a").on("click", function (e) {
+    if (this.hash !== "") {
+        e.preventDefault();
+        const hash = this.hash;
+        $("html, body").animate(
+            {
+                scrollTop: $(hash).offset().top - 120
+            },
+            600
+        );
+    }
+});
